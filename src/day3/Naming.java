d8af98274f80ea86d7fcb0136317282fbacf1cd2
ice <1040694044@qq.com>
2024-01-06 12:44:16 +0800

    is

--- src/day3/Naming.java
+++ src/day3/Naming.java
@@ -1,4 +1,21 @@
 package day3;
 
 public class Naming {
+    public static void main(String[] args) {
+        int age=2;
+        int code=2;
+        //不推荐
+        int $2=1;
+        int _3=2;
+        //不能以数字命名
+        //int 2a=2;
+        //已有的变量
+        //int public=2;
+
+
+
+
+    }
+
+
 }
