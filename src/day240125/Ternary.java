56e236c1498af95c751470e68d05f644e9f91204
ice <1040694044@qq.com>
2024-01-29 11:32:49 +0800

    第六天java补充

---
+++ src/day240125/Ternary.java
@@ -0,0 +1,12 @@
+package day240125;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 8;
+
+        int max = (x >= y) ? x : y;
+
+        System.out.println(max);
+    }
+}
