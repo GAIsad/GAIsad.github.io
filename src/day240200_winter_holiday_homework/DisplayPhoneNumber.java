7165b36955722bc1f6fd65be18fc45a0cd4d1f36
ice <1040694044@qq.com>
2024-02-21 10:18:56 +0800

    DisplayPhoneNumber - step 1
    写点注释，记录一下思考过程

---
+++ src/day240200_winter_holiday_homework/DisplayPhoneNumber.java
@@ -0,0 +1,19 @@
+package day240200_winter_holiday_homework;
+
+/**
+ * 问题描述：
+ * 已知 array1 和 array2
+ * <p>
+ * 要求：
+ * 输出隐藏数组中的电话号码
+ */
+public class DisplayPhoneNumber {
+    public static void main(String[] args) {
+        // 数据
+        int[] array1 = new int[]{8, 2, 1, 0, 3};
+        int[] array2 = new int[]{2, 0, 3, 2, 4, 0, 1, 3, 2, 3, 3};
+
+        // 思考过程
+        // 求的是电话号码，先不管这两个数组，正常的电话号码长什么样？
+    }
+}
