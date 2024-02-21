c0cb1f2700cbd75ebd61571749fd9d066c706052
ice <1040694044@qq.com>
2024-02-21 10:09:29 +0800

    Table99-step3
    行动态

--- src/day240200_winter_holiday_homework/Table99.java
+++ src/day240200_winter_holiday_homework/Table99.java
@@ -3,7 +3,7 @@ package day240200_winter_holiday_homework;
 public class Table99 {
     public static void main(String[] args) {
         for (int i = 1; i <=9; i++) {
-            System.out.println("1*1=1");
+            System.out.println("1*"+i+"=1");
         }
 
     }
