d3c8ab75b5bb02dddb35fabad01c1c954d5465ac
ice <1040694044@qq.com>
2024-02-21 11:49:20 +0800

    LinearSearch - step 0
    初始

---
+++ src/day240200_winter_holiday_homework/LinearSearch.java
@@ -0,0 +1,20 @@
+package day240200_winter_holiday_homework;
+
+/**
+ * 问题描述
+ *   已知数组：int[] arr = new int[]{10, 50, 30, 70, 80, 60, 20, 90, 40};
+ *   求：查找20是否在上述数组中出现过？如果出现，输出对应的索引值；否则输出没有找到
+ * <p>
+ * 预期输出
+ *   - 在index=**的位置上找到了**
+ *   - 没有找到**
+ */
+public class LinearSearch {
+    public static void main(String[] args) {
+        // 数据
+        int[] arr = new int[]{10, 50, 30, 70, 80, 60, 20, 90, 40};
+        int target = 20;
+
+        //
+    }
+}
