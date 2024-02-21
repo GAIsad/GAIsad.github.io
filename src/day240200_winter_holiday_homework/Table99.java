8b244cad625e2303dadb8b790cf379a00e35b092
ice <1040694044@qq.com>
2024-02-21 10:13:43 +0800

    Table99-step6
    改变行的列数

--- src/day240200_winter_holiday_homework/Table99.java
+++ src/day240200_winter_holiday_homework/Table99.java
@@ -3,7 +3,7 @@ package day240200_winter_holiday_homework;
 public class Table99 {
     public static void main(String[] args) {
         for (int i = 1; i <=9; i++) {
-            for (int j = 1; j <= 9; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("1 * " + i + " = 1");
 
             }
