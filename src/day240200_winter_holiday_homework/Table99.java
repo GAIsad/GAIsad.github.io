5f412b9dd6b53e3754676f2b24bc15278a92b817
ice <1040694044@qq.com>
2024-02-21 10:10:58 +0800

    Table99-step4
    9列

--- src/day240200_winter_holiday_homework/Table99.java
+++ src/day240200_winter_holiday_homework/Table99.java
@@ -3,7 +3,9 @@ package day240200_winter_holiday_homework;
 public class Table99 {
     public static void main(String[] args) {
         for (int i = 1; i <=9; i++) {
-            System.out.println("1*"+i+"=1");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("1 * " + i + " = 1");
+            }
         }
 
     }
