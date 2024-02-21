e40270ab0978a1dadb827e7f13cd76f442fbc42a
ice <1040694044@qq.com>
2024-02-21 10:15:04 +0800

    Table99-step7
    另一个数动态化

--- src/day240200_winter_holiday_homework/Table99.java
+++ src/day240200_winter_holiday_homework/Table99.java
@@ -4,7 +4,7 @@ public class Table99 {
     public static void main(String[] args) {
         for (int i = 1; i <=9; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("1 * " + i + " = 1");
+                System.out.print(j+"*" + i + " = 1");
 
             }
             System.out.println();
