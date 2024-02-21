536ca7eb3fefd24132a0bd56a3dbb623878c421c
ice <1040694044@qq.com>
2024-02-21 16:53:24 +0800

    BubbleSor -step5
    重复的进行应该用第二个循环

--- src/day240200_winter_holiday_homework/BubbleSor.java
+++ src/day240200_winter_holiday_homework/BubbleSor.java
@@ -32,21 +32,13 @@ public class BubbleSor {
         //第一轮：index 0~1比较，大者在后，之后1~2，2~3，3~4 依次比较（这是一个循环）循环结果是最大的数在最后一个
         //第二轮：index 0~1，1~2，2~3 ，依次比较，（循环）第二大的数在倒二个，3~4不用再次比较
         //多轮下去，不断将大的数放在后面，这时我们应该使用循环的嵌套，双重循环来进行，每轮重复内容
-        //第一轮
-        for (int j = 0; j < 5 - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                //交换过程
-                temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        //进行第二轮
-        for (int j = 0; j < arr.length-2; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
 
