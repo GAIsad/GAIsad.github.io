19c6b1a8679088c7458ba80dc1884fe534ee3c0c
ice <1040694044@qq.com>
2024-02-21 11:59:14 +0800

    LinearSearch - step 6
    处理没找到的情况

--- src/day240200_winter_holiday_homework/LinearSearch.java
+++ src/day240200_winter_holiday_homework/LinearSearch.java
@@ -2,24 +2,29 @@ package day240200_winter_holiday_homework;
 
 /**
  * 问题描述
- *   已知数组：int[] arr = new int[]{10, 50, 30, 70, 80, 60, 20, 90, 40};
- *   求：查找20是否在上述数组中出现过？如果出现，输出对应的索引值；否则输出没有找到
+ * 已知数组：int[] arr = new int[]{10, 50, 30, 70, 80, 60, 20, 90, 40};
+ * 求：查找20是否在上述数组中出现过？如果出现，输出对应的索引值；否则输出没有找到
  * <p>
  * 预期输出
- *   - 在index=**的位置上找到了**
- *   - 没有找到**
+ * - 在index=**的位置上找到了**
+ * - 没有找到**
  */
 public class LinearSearch {
     public static void main(String[] args) {
         // 数据
         int[] arr = new int[]{10, 50, 30, 70, 80, 60, 20, 90, 40};
-        int target = 20;
+
+        int target = 21;
+        boolean notFound = true;
         for (int i = 0; i < arr.length - 1; i++) {
             if (target == arr[i]) {
                 System.out.println("在index=" + i + "的位置上找到了" + target);
+                notFound = false;
                 break;
             }
-            System.out.println("--- ---");
+        }
+        if (notFound) {
+            System.out.println("没有找到" + target);
         }
     }
 }
