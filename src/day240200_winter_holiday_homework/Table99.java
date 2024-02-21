385ff11bb56b3d04cea021431fc7f99816033620
ice <1040694044@qq.com>
2024-02-21 10:07:56 +0800

    Table99-step2
    行重复

--- src/day240200_winter_holiday_homework/Table99.java
+++ src/day240200_winter_holiday_homework/Table99.java
@@ -2,6 +2,9 @@ package day240200_winter_holiday_homework;
 
 public class Table99 {
     public static void main(String[] args) {
-        System.out.println("1*1=1");
+        for (int i = 1; i <=9; i++) {
+            System.out.println("1*1=1");
+        }
+
     }
 }
