9e28d13c4d4396aa2674a75462a8f687b9b67cec
ice <1040694044@qq.com>
2024-02-21 15:36:02 +0800

    BinarySearch - step 0

---
+++ src/day240200_winter_holiday_homework/BinarySearch.java
@@ -0,0 +1,20 @@
+package day240200_winter_holiday_homework;
+
+/**
+ * 问题描述
+ * 已知数组：int[] arr = new int[]{3, 4, 5, 6, 7, 8, 9};
+ * 额外信息：数组元素是有顺序
+ * 求：查找8是否在上述数组中出现过？如果出现，输出对应的索引值；否则输出没有找到
+ * 额外要求：必须使用二分法查找，以便提升查找效率
+ * <p>
+ * 预期输出
+ * - 在index=**的位置上找到了**
+ * - 没有找到**
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        // 数据（有序）
+        int[] arr = new int[]{3, 4, 5, 6, 7, 8, 9};
+        int target = 8;
+    }
+}
