844096c79f639807216002fab7b4d049ed7ec924
ice <1040694044@qq.com>
2024-02-22 11:53:28 +0800

    BubbleSor -step8
    优化：：增加一个变量，判断此轮是否进行交换，如果没有交换，结束循环

--- src/day240200_winter_holiday_homework/BubbleSor.java
+++ src/day240200_winter_holiday_homework/BubbleSor.java
@@ -56,12 +56,17 @@ public class BubbleSor {
                     arr[j] = temp;}
          */
         for (int i = 1; i < arr.length; i++) {
+            boolean swapped = false;
             for (int j = 0; j < arr.length-i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
 
