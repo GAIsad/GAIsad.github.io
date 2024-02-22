8ec72bda532e84a65f45e26469d06ecac567072b
ice <1040694044@qq.com>
2024-02-22 14:15:11 +0800

    BubbleSor_two -step8
    外循环出现

--- src/day240200_winter_holiday_homework/BubbleSor_two.java
+++ src/day240200_winter_holiday_homework/BubbleSor_two.java
@@ -19,19 +19,13 @@ public class BubbleSor_two {
         }
         System.out.println();
         for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+            for (int j = 0; j < (arr.length - 1) - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-            }
-        }
-        for (int i = 0; i < arr.length - 2; i++) {
-            if (arr[i] > arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-
+                }
             }
         }
         for (int i = 0; i < arr.length; i++) {
