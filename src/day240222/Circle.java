9adaaeef64c5de8aec6c61fb8af6617d41bcdf4c
ice <1040694044@qq.com>
2024-02-23 15:24:32 +0800

    Circle第一版本

--- src/day240222/Circle.java
+++ src/day240222/Circle.java
@@ -2,17 +2,10 @@ package day240222;
 
 public class Circle {
     //state
-    Double pi= 3.14;
-    private double radius;
+   double radius;
     //behavior
-    public void setradius(double setradius){
-        radius=setradius;
-        if(radius<0){
-            System.out.println("错误的输入");
-        }
+    public Circle(){
+        System.out.println("输出");
+    }
 
-    }
-    public String getradius(){
-        return "圆的面积是"+(pi*radius*radius);
-    }
 }
