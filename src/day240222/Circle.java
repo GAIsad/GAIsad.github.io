0b59f794e3ca79dec052ec7564f3c0b6f2233734
ice <1040694044@qq.com>
2024-02-23 15:26:50 +0800

    Circle第一版本

--- src/day240222/Circle.java
+++ src/day240222/Circle.java
@@ -4,8 +4,9 @@ public class Circle {
     //state
    double radius;
     //behavior
-    public Circle(){
-        System.out.println("输出");
-    }
+    //无参的构造方法
 
+    public double area(){
+        return 3.14*radius*radius;
+    }
 }
