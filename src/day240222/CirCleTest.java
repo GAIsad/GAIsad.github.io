397201e51d28a4e21553eda2d0625725b6c129d4
ice <1040694044@qq.com>
2024-02-23 16:52:05 +0800

    Circle有参时new类名需补充成员函数

--- src/day240222/CirCleTest.java
+++ src/day240222/CirCleTest.java
@@ -4,8 +4,8 @@ package day240222;
 
 public class CirCleTest {
     public static void main(String[] args) {
-        Circle circle=new Circle();
-        circle.radius=5;
+        Circle circle=new Circle(5);
+
         System.out.println(circle.area());
     }
 }
--- src/day240222/Circle.java
+++ src/day240222/Circle.java
@@ -3,8 +3,8 @@ package day240222;
 public class Circle {
     //state
    double radius;
-    public Circle(){
-        System.out.println("输出");
+    public Circle(double r){
+        radius=r;
     }
     //behavior
 
