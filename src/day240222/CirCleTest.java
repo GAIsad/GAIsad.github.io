e3f3b87ae9e6276ab07a3201dedc9779592ab2a2
ice <1040694044@qq.com>
2024-02-23 16:48:24 +0800

    Circle的第一个版本

--- src/day240222/CirCleTest.java
+++ src/day240222/CirCleTest.java
@@ -1,14 +1,11 @@
 package day240222;
 
-import java.util.Scanner;
+
 
 public class CirCleTest {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("请输出圆的半径");
-        double radius=scanner.nextDouble();
-        Circle circle = new Circle();
-        circle.setradius(radius);
-        System.out.println(circle.getradius());
+        Circle circle=new Circle();
+        circle.radius=5;
+        System.out.println(circle.area());
     }
 }
--- src/day240222/Circle.java
+++ src/day240222/Circle.java
@@ -3,9 +3,8 @@ package day240222;
 public class Circle {
     //state
    double radius;
+
     //behavior
-    //无参的构造方法
-    //
 
 
     public double area(){
