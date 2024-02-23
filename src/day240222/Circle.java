c00dd264581e3a53314c52f938ba56c537059a62
ice <1040694044@qq.com>
2024-02-23 16:57:09 +0800

    pi加入static修饰，是pi属于类而不是对象

--- src/day240222/Circle.java
+++ src/day240222/Circle.java
@@ -2,8 +2,8 @@ package day240222;
 
 public class Circle {
     //state
-    double pi=3.14;
-    private  double radius;
+    private static double pi=3.14;
+      double radius;
     public Circle(double radius){
         // IDEA 编辑器自动生成的版本
         this.radius=radius;
