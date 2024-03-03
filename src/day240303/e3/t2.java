c91378f20bbbfea68d798ad2929f6344c8dbda55
ice <1040694044@qq.com>
2024-03-03 19:26:40 +0800

    overload重载原则（相同的方法相同的变量）

---
+++ src/day240303/e3/t2.java
@@ -0,0 +1,24 @@
+package day240303.e3;
+
+public class t2 {
+    //overload
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static double sum(double a,double b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1=new int[]{1,23,3};
+        System.out.println(arr1);
+        //输出地址
+        char[] arr2=new char[]{'a','b','c','d','e'};
+        System.out.println(arr2);
+        //输出数组
+        boolean[] arr3=new boolean[]{false,true,true};
+        System.out.println(arr3);
+        //输出地址
+
+    }
+}
