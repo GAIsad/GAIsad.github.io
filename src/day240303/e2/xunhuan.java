3b069c0a08fda354914859007bf345ce7cebd5ee
ice <1040694044@qq.com>
2024-03-03 19:25:38 +0800

    for循环的一种未了解的新形式

---
+++ src/day240303/e2/xunhuan.java
@@ -0,0 +1,12 @@
+package day240303.e2;
+
+public class xunhuan {
+    public static void main(String[] args) {
+
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int sc : numbers) {
+            System.out.println(sc);
+        }
+    }
+}
