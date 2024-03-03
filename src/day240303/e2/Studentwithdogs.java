51a019d9506ac0774446eb7d4fa18ea1ff15583d
ice <1040694044@qq.com>
2024-03-03 11:38:52 +0800

    Studentwithdog-step7 方法内容多

--- src/day240303/e2/Studentwithdogs.java
+++ src/day240303/e2/Studentwithdogs.java
@@ -7,6 +7,7 @@ public class Studentwithdogs {
     Dog[] dogs;
     public void show(){
         System.out.println("学生(学号"+id+")的信息：学号="+id+" "+"姓名="+name+" "+"年龄="+age+" "+"\n"+"有"+dogs.length+"只狗狗");
+        //一个方法只做一件事
         for(Dog dog:dogs){
             dog.show();
         }
