23f88fe0456a245d8e65d293ce11b29c1b7aba74
ice <1040694044@qq.com>
2024-03-03 11:28:20 +0800

    Studentwithdog-step4 改用数组

--- src/day240303/e2/Studentwithdogs.java
+++ src/day240303/e2/Studentwithdogs.java
@@ -4,7 +4,7 @@ public class Studentwithdogs {
     String id;
     String name;
     String age;
-    String dogs;
+    Dog[] dogs;
     public void show(){
         System.out.println("学生(学号"+id+")的信息：学号="+id+" "+"姓名="+name+" "+"年龄="+age+" "+"\n"+"有"+dogs+"只狗狗");
     }
--- src/day240303/e2/StudentwithdogsTest2.java
+++ src/day240303/e2/StudentwithdogsTest2.java
@@ -22,14 +22,14 @@ public class StudentwithdogsTest2 {
         studentwithdogs1.id = "202321396113";
         studentwithdogs1.name = "哈多啦";
         studentwithdogs1.age = "18";
-        studentwithdogs1.dogs = "1";
+        studentwithdogs1.dogs =new Dog[]{dog1};
         studentwithdogs1.show();
         dog1.show();
         Studentwithdogs studentwithdogs2 = new Studentwithdogs();
         studentwithdogs2.id = "202321396114";
         studentwithdogs2.age = "18";
         studentwithdogs2.name = "笋子";
-        studentwithdogs2.dogs = "2";
+        studentwithdogs2.dogs = new Dog[]{dog2,dog3};
         studentwithdogs2.show();
         //DRY
         dog2.show();
