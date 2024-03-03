9e798214290b3828fe4801d64c276429c1ef1171
ice <1040694044@qq.com>
2024-03-03 11:08:43 +0800

    完善实现功能student with dogs

--- src/day240303/e2/Studentwithdogs.java
+++ src/day240303/e2/Studentwithdogs.java
@@ -6,6 +6,6 @@ public class Studentwithdogs {
     String age;
     String dogs;
     public void show(){
-        System.out.println("学生(学号"+id+")的信息：学号="+id+" "+"姓名="+name+" "+"年龄="+age+" "+"狗狗们"+dogs);
+        System.out.println("学生(学号"+id+")的信息：学号="+id+" "+"姓名="+name+" "+"年龄="+age+" "+"\n"+"有"+dogs+"只狗狗");
     }
 }
--- src/day240303/e2/StudentwithdogsTest2.java
+++ src/day240303/e2/StudentwithdogsTest2.java
@@ -6,15 +6,15 @@ public class StudentwithdogsTest2 {
     public static void main(String[] args) {
 
         Dog dog1 = new Dog();
-        dog1.id =12;
+        dog1.id =1;
         dog1.name="小黑";
         dog1.color="black";
         Dog dog2 = new Dog();
-        dog2.id =12;
+        dog2.id =2;
         dog2.name="小白";
         dog2.color="white";
         Dog dog3 = new Dog();
-        dog3.id =12;
+        dog3.id =3;
         dog3.name="小红";
         dog3.color="red";
 
@@ -31,6 +31,7 @@ public class StudentwithdogsTest2 {
         studentwithdogs2.name = "笋子";
         studentwithdogs2.dogs = "2";
         studentwithdogs2.show();
+
         dog2.show();
         dog3.show();
     }
