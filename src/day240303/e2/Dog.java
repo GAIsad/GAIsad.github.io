86f88277c49b066060df13dea8a386826440fe57
ice <1040694044@qq.com>
2024-03-03 10:39:09 +0800

    dog-e2 狗类别的完成

---
+++ src/day240303/e2/Dog.java
@@ -0,0 +1,11 @@
+package day240303.e2;
+
+public class Dog {
+    int id;
+    String color;
+    String name;
+    void show(){
+        System.out.println("狗（编号"+id+")的信息:编号="+id+"、名字="+name+"、颜色="+color);
+
+    }
+}
