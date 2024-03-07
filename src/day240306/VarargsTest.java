766420584e0c2ffd6545c394370d395e434dc15d
ice <1040694044@qq.com>
2024-03-07 11:08:11 +0800

    不定数量的参数方法

--- src/day240306/VarargsTest.java
+++ src/day240306/VarargsTest.java
@@ -1,18 +1,15 @@
 package day240306;
 
 public class VarargsTest {
-    public int add(int[] numbers) {
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
-        return sum;
+    public  static int add(int...a){
+
+        return 0;
     }
 
+
     public static void main(String[] args) {
-        VarargsTest adder = new VarargsTest();
-        int[] numbers = {1, 2, 3, 4, 5};  // 你要相加的数字数组
-        int sum = adder.add(numbers);
-        System.out.println("数组的和是：" + sum);
+        System.out.println(add(1));
+        System.out.println(add(1,2,3));
+        System.out.println(add(1,2,3,9));
     }
 }
