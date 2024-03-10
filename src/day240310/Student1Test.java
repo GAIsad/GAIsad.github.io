dd08f7bc2e1067b68d774b30c623b8a9027c2d1d
ice <1040694044@qq.com>
2024-03-10 11:12:52 +0800

    encapsulation - singleton basic - private - instance-值可被改变

--- src/day240310/Student1Test.java
+++ src/day240310/Student1Test.java
@@ -4,7 +4,9 @@ public class Student1Test {
     public static void main(String[] args) {
 
         Student1 student =Student1.instance;
+
         //Student1 student1=new Student1();
+        Student1.instance=null;
         Student1 student2 =Student1.instance;
         System.out.println(student);
         System.out.println(student2);
