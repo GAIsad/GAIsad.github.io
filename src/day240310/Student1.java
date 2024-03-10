748eb0024acc5480eac08b5c21c9792374d8d155
ice <1040694044@qq.com>
2024-03-10 11:24:10 +0800

    encapsulation - singleton basic - private - final
    instance的值不需要改变

--- src/day240310/Student1.java
+++ src/day240310/Student1.java
@@ -3,7 +3,7 @@ package day240310;
 public class Student1 {
     // state
     String name;
-    private static Student1 instance = new Student1();
+    private static final Student1 instance = new Student1();
 
 
     private Student1() {
