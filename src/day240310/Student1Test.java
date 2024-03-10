2a6eea376e20f73f98ba973b6ffa35060a2b262e
ice <1040694044@qq.com>
2024-03-10 10:57:57 +0800

    encapsulation - singleton basic - private - 思考
    把构造器私有化 -> 问题 -> 外部不能 new -> 在哪里 new?

---
+++ src/day240310/Student1Test.java
@@ -0,0 +1,10 @@
+package day240310;
+
+public class Student1Test {
+    public static void main(String[] args) {
+
+
+        //Student1 student1=new Student1();
+        //System.out.println(student1);
+    }
+}
