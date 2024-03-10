cb5f888941335d0227404c6b0c82d9c10568f20d
ice <1040694044@qq.com>
2024-03-10 11:22:38 +0800

    encapsulation - singleton basic - private - instance-解决上一个问题值-step2
    用privtate修饰同时，增加getinstance的保护

--- src/day240310/Student1.java
+++ src/day240310/Student1.java
@@ -8,5 +8,10 @@ public class Student1 {
 
     private Student1() {
         System.out.println("----");
+
+    }
+    //static
+    public static Student1 getInstance(){
+        return instance;
     }
 }
