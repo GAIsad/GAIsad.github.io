d78d524d7d4d21dcddf2189362ff94024fc8cb95
ice <1040694044@qq.com>
2024-03-17 16:52:10 +0800

    扑克补充

--- src/day240317/Pokers.java
+++ src/day240317/Pokers.java
@@ -1,12 +1,15 @@
 package day240317;
 
 public class Pokers {
-    public static String[] color = {"梅花", "黑桃", "方片","红桃"};
-    public static String[] number={"1","2","3","4","5","6","7","8","9","10","j","q","k"};
-    void getPoker(){
-        for (int i = 0; i <=color.length-1; i++) {
+    String number1;
+    String color1;
+    public static String[] color={"梅花", "黑桃", "方片","红桃"};
+    public static String[] number={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+
+    public void getPoker(){
+        for (int i = 0; i <=3; i++) {
             System.out.println();
-            for (int j = 0; j <= number.length-1; j++) {
+            for (int j = 0; j <=12; j++) {
                 System.out.print(color[i]+number[j]+" ");
             }
         }
