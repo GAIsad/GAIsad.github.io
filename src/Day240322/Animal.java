a204ad1354ed510a23287d836e02ddb6783de1c3
ice <1040694044@qq.com>
2024-03-22 19:41:28 +0800

    重写（override）——重写的方法与子类重写的方法的方法名和形参类types必须相同

---
+++ src/Day240322/Animal.java
@@ -0,0 +1,7 @@
+package Day240322;
+
+public class Animal {
+    public void eat(int a) {
+        System.out.println("This animal eats food.");
+    }
+}
---
+++ src/Day240322/Dog.java
@@ -0,0 +1,7 @@
+package Day240322;
+
+public class Dog extends Animal{
+    public void eat(int a) {
+        System.out.println("The dog eats bones.");
+    }
+}
