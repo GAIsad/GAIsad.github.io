31a19b1d1bdef4e8e6db8821b7f1cfad4884f4a5
ice <1040694044@qq.com>
2024-03-22 20:23:13 +0800

    重写（override）————规则6
    父类返回类型是引用数据类型，子类重写方法的返回值类型必须与被重写的方法返回值类型相同或被重写方法的返回值类型的子类

--- src/Day240322/Animal.java
+++ src/Day240322/Animal.java
@@ -1,6 +1,7 @@
 package Day240322;
 
 public class Animal {
+
     public String  eat() {
         System.out.println("This animal eats food.");
         return "";
--- src/Day240322/Dog.java
+++ src/Day240322/Dog.java
@@ -1,8 +1,10 @@
 package Day240322;
 
 public class Dog extends Animal {
+    int Dog;
         public String eat() {
             System.out.println("The dog eats bones.");
+
             return "";
         }
 }
