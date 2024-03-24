3012ce25dcfba7f9e068ee7127feb74cb20d600f
ice <1040694044@qq.com>
2024-03-24 15:34:22 +0800

    e-1-每个人都有自己的孤独

---
+++ src/Day240324/e/Person.java
@@ -0,0 +1,12 @@
+package Day240324.e;
+
+public class Person {
+    //state
+    String name;
+
+    //behavior
+    public void describeLonely(){
+        System.out.println("   ");//可重写
+    }
+
+}
---
+++ src/Day240324/e/Student.java
@@ -0,0 +1,8 @@
+package Day240324.e;
+
+public class Student extends Person{
+    @Override
+    public void describeLonely() {
+        System.out.println("Student.describeLonely");//想到提参数
+    }
+}
