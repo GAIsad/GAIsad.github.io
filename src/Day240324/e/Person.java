1d45068aa5c5fe2054900d5b832135f20c7db92f
ice <1040694044@qq.com>
2024-03-24 15:59:03 +0800

    e-7-add judgeXDD

--- src/Day240324/e/Person.java
+++ src/Day240324/e/Person.java
@@ -3,12 +3,16 @@ package Day240324.e;
 public abstract class Person {
     //state
     String name;
+
     public Person(String name) {
-        this.name=name;
+        this.name = name;
     }
 
 
     //behavior
     public abstract void describeLonely(String text);//抽象类对应抽象方法体
 
+    public void judgeXDD(String text) {
+        System.out.println("ddd");
+    }
 }
