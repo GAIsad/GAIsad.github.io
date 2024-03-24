40764a266da77ca658be3d258fb9e22b0c007fea
ice <1040694044@qq.com>
2024-03-24 10:03:27 +0800

    Person with animal -step2
    人adopt猫和狗

---
+++ src/Day240324/Test1/Dog.java
@@ -0,0 +1,15 @@
+package Day240324.Test1;
+
+public class Dog {
+    //state
+    String name;
+    //constructor
+    public  Dog(String name){
+        this.name=name;
+    }
+
+
+
+
+    //behavior
+}
--- src/Day240324/Test1/Test.java
+++ src/Day240324/Test1/Test.java
@@ -4,6 +4,8 @@ public class Test {
     public static void main(String[] args) {
         Person person=new Person("小张");
         Cat cat=new Cat("浩浩");
+        Dog dog=new Dog("小白");
         person.adopt(cat);
+        person.adopt(dog);
     }
 }
