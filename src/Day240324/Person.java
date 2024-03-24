bfe65ba1b205830cd0144249dfdc1d07f06607ec
ice <1040694044@qq.com>
2024-03-24 09:22:27 +0800

    step3-收养猫狗类

--- src/Day240324/Person.java
+++ src/Day240324/Person.java
@@ -3,11 +3,31 @@ package Day240324;
 public class Person {
     //state
     String name;
-    int number;
+    int numberCat;
+    int numberDog;
+    String nameCat;
+    String nameDog;
     //behavior
-    void supportCat(){
-        System.out.println(name+"养了"+number+"只猫");
+    public int getNumberCat() {
+        return numberCat;
     }
 
+    public void setNumberCat(int numberCat) {
+        this.numberCat = numberCat;
+    }
+    public int getNumberDog() {
+        return numberDog;
+    }
+
+    public void setNumberDog(int numberDog) {
+        this.numberDog = numberDog;
+    }
+
+    void adoptedCat(){
+        System.out.println(name+"养了"+numberCat+"只猫"+"猫叫："+nameCat);
+    }
+    void adoptedDog(){
+        System.out.println(name+"养了"+numberDog+"只狗"+"狗叫:"+nameDog);
+    }
 
 }
