5ec5857f2ea75e4a5ecc8d10c9fd7e9b149d7f2f
ice <1040694044@qq.com>
2024-03-24 09:17:17 +0800

    step2-实现猫类方法

--- src/Day240324/Person.java
+++ src/Day240324/Person.java
@@ -2,6 +2,12 @@ package Day240324;
 
 public class Person {
     //state
-
+    String name;
+    int number;
     //behavior
+    void supportCat(){
+        System.out.println(name+"养了"+number+"只猫");
+    }
+
+
 }
