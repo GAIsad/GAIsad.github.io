90cf31bebd9399e0435a57ac45ac1b033826f2e0
ice <1040694044@qq.com>
2024-03-24 15:41:33 +0800

    e-3-每个人都有自己的孤独-多人孤独的语言

--- src/Day240324/e/Student.java
+++ src/Day240324/e/Student.java
@@ -1,8 +1,12 @@
 package Day240324.e;
 
 public class Student extends Person{
+    public Student(String name) {
+    }
+
     @Override
     public void describeLonely(String text) {
+
         System.out.println(text);//想到提参数
     }
 }
---
+++ src/Day240324/e/Test.java
@@ -0,0 +1,12 @@
+package Day240324.e;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        Student student = new Student("芙芙");
+        student.describeLonely("欢唱");
+        Student student1=new Student("纳纳");
+        student1.describeLonely("当当");
+    }
+}
