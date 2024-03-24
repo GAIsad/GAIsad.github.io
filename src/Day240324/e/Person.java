e2527411f258fe813e9ef66c7b9312249614e635
ice <1040694044@qq.com>
2024-03-24 15:44:44 +0800

    e-4-Person-可以不做

--- src/Day240324/e/Person.java
+++ src/Day240324/e/Person.java
@@ -3,6 +3,10 @@ package Day240324.e;
 public class Person {
     //state
     String name;
+    public Person(String name) {
+        this.name=name;
+    }
+
 
     //behavior
     public void describeLonely(String text){
